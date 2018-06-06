11015d79249cb7dd3fec11249bf55866c76547f7
myaTheingi <myaTheingi>
2018-06-06 13:52:08 +0530

    Added ex11.py

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+x=38
+age = input(x)
+print("How tall are you?", end=' ')
+y="6'2\""
+height = input(y)
+print("How much do you weight?", end=' ')
+z="180lbs"
+weight = input(z)
+print(f"So, you're {x} old, {y} tall and {z} heavy.")
